cf5da34ad36764b2b1dc3c4761570035d40702fb
medflick <143778423+medflick@users.noreply.github.com>
2023-12-04 13:29:27 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -10,4 +10,3 @@ npm run build
 sudo chown www-data:www-data -R /var/www/html/medflick2/.*
 sudo chmod 777 -R /var/www/html/medflick2/.*
 sudo npm install -g pm2
-pm2 start npm --name app1 -- run start -- -p 3000
