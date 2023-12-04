fede6c6f8e2d6f4d1abb3e45149da2b0483958ee
medflick <143778423+medflick@users.noreply.github.com>
2023-12-04 10:40:02 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. /.nvm/nvm.sh
-nvm install v18
-export NVM_DIR="/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 cd /var/www/html/medflick2
 sudo chown www-data:www-data -R /var/www/html/medflick2/
 sudo chmod 777 -R /var/www/html/medflick2/
@@ -15,6 +9,5 @@ sudo chmod 777 -R /var/www/html/medflick2/.*
 npm run build
 sudo chown www-data:www-data -R /var/www/html/medflick2/.*
 sudo chmod 777 -R /var/www/html/medflick2/.*
-npm install -g pm2
-pm2 delete app1
+sudo npm install -g pm2
 pm2 start npm --name app1 -- run start -- -p 3000
