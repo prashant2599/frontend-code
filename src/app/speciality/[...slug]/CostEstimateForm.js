e6385b6fd3a1107dbb968c63c29ace99db49ea42
manishkv2000@gmail.com <manishkv2000@gmail.com>
2023-12-28 15:25:49 +0530

    changes

--- src/app/speciality/[...slug]/CostEstimateForm.js
+++ src/app/speciality/[...slug]/CostEstimateForm.js
@@ -238,7 +238,7 @@ const CostEstimateForm = ({ specialityId }) => {
       };
 
       // Replace 'YOUR_API_ENDPOINT' with your actual API endpoint
-      const apiEndpoint = `${process.env.NEXT_PUBLIC_BASE_URL}/api/free_quote_treatment`;
+      const apiEndpoint = `https://api.medflick.com/api/free_quote_treatment`;
 
       setIsLoading(true);
 
