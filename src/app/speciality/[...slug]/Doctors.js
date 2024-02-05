d79e525c452c40db1c5e2c8fa14cf430292d1c57
manishkv2000@gmail.com <manishkv2000@gmail.com>
2024-02-05 18:42:08 +0530

    dsa

--- src/app/speciality/[...slug]/Doctors.js
+++ src/app/speciality/[...slug]/Doctors.js
@@ -38,7 +38,7 @@ const Doctors = ({ doctor, category, categoryName, slugs }) => {
               <div className="item" key={e.id}>
                 <div className="item-home-expert">
                   <Link href={`/doctor/${e.slug}`}>
-                    <img src={`https://dev.medflick.com/doctor/${e.image}`} />
+                    <img src={`https://dev.medflick.com/doctor/${e.icon}`} />
                   </Link>
                 </div>
                 <div className="home-expert-text">
@@ -50,14 +50,14 @@ const Doctors = ({ doctor, category, categoryName, slugs }) => {
                         </h3>
                       </Link>
                     </div>
-                    {/* <div className="home-page-hospital-logo">
+                    <div className="home-page-hospital-logo">
                       <Link href={`/hospital/${e.hospital_slug}`}>
                         <img
                           src={`https://dev.medflick.com/hospital/${e.hospitalicon}`}
                           className="home-doctor-hospital-logo"
                         />
                       </Link>
-                    </div> */}
+                    </div>
                   </div>
                   <p>
                     {e.dept.length > 25 ? `${e.dept.slice(0, 25)}..` : e.dept}
